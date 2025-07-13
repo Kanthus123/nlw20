b6cad852dcf3a9980470d9f8aa00bed1baf80257
Kanthus123 <paulo.csm@outlook.com>
2025-07-13 20:06:03 -0300

    feat: add gemini integration, add ability to save audio file and use gemini to answer the questions

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,9 +1,11 @@
 // Barrel file -> arquivo que re-exporta todos os arquivos dentro dele
 
+import { audioChunks } from "./audio-chunks.ts";
 import { questions } from "./questions.ts";
 import { rooms } from "./rooms.ts";
 
 export const schema = {
     rooms,
-    questions
+    questions,
+    audioChunks,
 }
